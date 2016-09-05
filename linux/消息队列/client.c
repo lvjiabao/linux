570ecd8cef66c7259b77876db1dcf46ec80a25ad
吕嘉宝 <1208542112@qq.com>
2016-09-05 23:50:04 +0800

    Update client.c

--- linux/消息队列/client.c
+++ linux/消息队列/client.c
@@ -15,8 +15,8 @@ int main()
 	{
 		memset(buf,0,sizeof(buf));
 		printf("please enter:");
-		fflush(stdout);///////
-		ssize_t size=read(0,buf,sizeof(buf));
+		fflush(stdout);///////因为没有\n，所以会输出到缓冲中去
+		ssize_t size=read(0,buf,sizeof(buf));//
 		if(size>0)
 			buf[size-1]=0;
 		send_msg(id,CLIENT_T,buf);
